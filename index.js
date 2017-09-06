6356c676eb2337f3a0caa5073f6181210cbd2880
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-06 20:03:33 +0700

    xong

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const reload = require('reload'); // eslint-disable-line
+// middleware
+const bodyParser = require('body-parser');
+const parser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
 
@@ -7,6 +10,11 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('home'));
+
+app.post('/signin', parser, (req, res) => {
+    console.log(req.body);
+    res.send('Xin chao');
+});
 
 reload(app);
 
